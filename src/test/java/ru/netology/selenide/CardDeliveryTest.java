eff64180a14b35bf4b58f3cd3c3a558788a41fa1
Yudin Egor <yudin.egor26@gmail.com>
2021-09-23 23:01:12 +0500

    test add

---
+++ src/test/java/ru/netology/selenide/CardDeliveryTest.java
@@ -0,0 +1,33 @@
+package ru.netology.selenide;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+
+    String dateFormation(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void shouldTest() {
+        String dateMeeting = dateFormation(4);
+
+        open("http://localhost:9999");
+        $("[placeholder='Город']").setValue("Екатеринбург");
+        $("[data-test-id='date'] [type='tel']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] [type='tel']").setValue(dateMeeting);
+        $("[name='name']").setValue("Петр Паль");
+        $("[name='phone']").setValue("+79156235689");
+        $("[data-test-id='agreement']").click();
+        $(byText("Забронировать")).click();
+        $(".notification__title").shouldHave(exactText("Успешно!"), Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + dateMeeting));
+    }
+}
